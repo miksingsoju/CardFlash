2c12865094432e56416facbf6fbccef83605f83e
miksingsoju <161931661+miksingsoju@users.noreply.github.com>
2025-05-18 21:59:39 +0800

    Init Flashcard App

---
+++ flashcard/urls.py
@@ -0,0 +1,21 @@
+from django.urls import path
+
+urlpatterns = [
+    # main page
+    # subject list
+        #  deck list
+        #  deck view
+        #  card view
+    
+    # add subject
+    # edit dashboard
+
+    # add deck
+    # edit deck
+
+    # add flashcard
+    # edit flashcard
+
+]
+
+app_name = "flashcard"
